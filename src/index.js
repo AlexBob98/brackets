ed73461cef582b541b46a90690bced5d31c75761
Alexander Bobryshev <94990187+AlexBob98@users.noreply.github.com>
2022-02-16 23:25:27 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracket) {
+  let arr = [];
+for (let e of str) {
+    if (e != new Map(bracket).get(arr[arr.length - 1])) {
+      arr.push(e)
+    } else 
+    arr.pop(e)
+  }
+  return arr.length === 0
 }
